f8baa1c1dc4ec7ce53936575911e0405faae8263
José Luis Segura Lucas <joseluis@redhat.com>
2022-03-24 13:19:05 +0100

    Moving to Gitlab. Using black

--- insights_sha_extractor/utils/clowder.py
+++ insights_sha_extractor/utils/clowder.py
@@ -21,14 +21,10 @@ def apply_clowder_config(manifest):
     """Apply Clowder config values to ICM config manifest."""
     Loader = getattr(yaml, "CSafeLoader", yaml.SafeLoader)
     config = yaml.load(manifest, Loader=Loader)
-    kafka_url = (
-        f"{LoadedConfig.kafka.brokers[0].hostname}:{LoadedConfig.kafka.brokers[0].port}"
-    )
+    kafka_url = f"{LoadedConfig.kafka.brokers[0].hostname}:{LoadedConfig.kafka.brokers[0].port}"
     config["service"]["consumer"]["kwargs"]["bootstrap_servers"] = kafka_url
     config["service"]["publisher"]["kwargs"]["bootstrap_servers"] = kafka_url
-    pt_watcher = (
-        "ccx_messaging.watchers.payload_tracker_watcher.PayloadTrackerWatcher"
-    )
+    pt_watcher = "ccx_messaging.watchers.payload_tracker_watcher.PayloadTrackerWatcher"
     for watcher in config["service"]["watchers"]:
         if watcher["name"] == pt_watcher:
             watcher["kwargs"]["bootstrap_servers"] = kafka_url
