f29902f47a02d484586184247ec4c0ab2c5780d1
Martin Zibricky <527280+matysek@users.noreply.github.com>
2024-05-27 14:37:21 +0200

    [CCXDEV-12804] Add to pr_check running sanity check for ocp-vulnerability tests

--- pr_check.sh
+++ pr_check.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
+# Copyright 2024 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exv
 
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-APP_NAME="ocp-vulerability"  # name of app-sre "application" folder this component lives in
+APP_NAME="ocp-vulnerability"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="sha-extractor"
 IMAGE="quay.io/cloudservices/sha-extractor"
 COMPONENTS="sha-extractor"  # space-separated list of components to laod
 COMPONENTS_W_RESOURCES="sha-extractor"  # component to keep
-CACHE_FROM_LATEST_IMAGE="false"
+CACHE_FROM_LATEST_IMAGE="true"
+DEPLOY_FRONTENDS="false"
 
-# export IQE_PLUGINS="ocp-vulnerability"
-# export IQE_MARKER_EXPRESSION="smoke" # ccx_data_pipeline_smoke does not exits (at least yet) as marker in the plugin
-# export IQE_FILTER_EXPRESSION=""
-# export IQE_REQUIREMENTS_PRIORITY=""
-# export IQE_TEST_IMPORTANCE=""
-# export IQE_CJI_TIMEOUT="30m"
+export IQE_PLUGINS="ocp-vulnerability"
+export IQE_MARKER_EXPRESSION=""
+export IQE_FILTER_EXPRESSION="test_plugin_accessible"
+export IQE_REQUIREMENTS_PRIORITY=""
+export IQE_TEST_IMPORTANCE=""
+export IQE_CJI_TIMEOUT="30m"
 
 function build_image() {
     source $CICD_ROOT/build.sh
@@ -25,10 +41,9 @@ function deploy_ephemeral() {
     source $CICD_ROOT/deploy_ephemeral_env.sh
 }
 
-# TODO: Uncomment when smoke tests are created
 function run_smoke_tests() {
     # component name needs to be re-export to match ClowdApp name (as bonfire requires for this)
-    # export COMPONENT_NAME="ccx-sha-extractor"
+    # export COMPONENT_NAME="sha-extractor"
     # source $CICD_ROOT/cji_smoke_test.sh
     echo "To be implemented"
 }
@@ -43,11 +58,10 @@ build_image
 echo "deploying to ephemeral"
 deploy_ephemeral
 
-# TODO: Uncomment when smoke tests are created
 echo "running PR smoke tests"
 run_smoke_tests
 
 # Temporary stub
-mkdir -p artifacts
-TIMESTAMP=`date +%Y-%m-%dT%H:%M:%S.%N`
-echo '<?xml version="1.0" encoding="utf-8"?><testsuites><testsuite name="pytest" errors="0" failures="0" skipped="0" tests="1" time="0.000" timestamp="'${TIMESTAMP}'" hostname="'${HOST}'"><testcase classname="test" name="test_stub" time="0.000" /></testsuite></testsuites>' > artifacts/junit-stub.xml
+#mkdir -p artifacts
+#TIMESTAMP=`date +%Y-%m-%dT%H:%M:%S.%N`
+#echo '<?xml version="1.0" encoding="utf-8"?><testsuites><testsuite name="pytest" errors="0" failures="0" skipped="0" tests="1" time="0.000" timestamp="'${TIMESTAMP}'" hostname="'${HOST}'"><testcase classname="test" name="test_stub" time="0.000" /></testsuite></testsuites>' > artifacts/junit-stub.xml
